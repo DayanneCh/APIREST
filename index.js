c3f7ca9967972dcac39dbc4e6d100a6d3e5ec9ec
DayanneCh <dashilatra.sena@gmail.com>
2025-06-18 20:48:29 -0500

    Se modifico el archivo index.js y se creo la conexión a la base de datos.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express =require('express');
 const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
-app.get ('/', (req, res)=>{
-    res.send('servidor funcionando correctamente');
+const postRoute = require ('./routes/post');
+app.use('/servicios', postRoute);
+
+mongoose.connect('mongodb+srv://dashilatrasena:EzbOxZEmdJRPjZFv@apievidencia.ls3snwv.mongodb.net/post?retryWrites=true&w=majority&appName=APIevidencia',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
-
 app.listen(10000);
