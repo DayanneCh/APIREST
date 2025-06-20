d150e22228b355bc9d6455edc021758e214a9008
DayanneCh <dashilatra.sena@gmail.com>
2025-06-19 22:50:44 -0500

    se completo la primera API

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-const postRoute = require ('./routes/post');
+const postRoute = require ('./routes/post'); //conexion con las rutas
 app.use('/servicios', postRoute);
 
 mongoose.connect('mongodb+srv://dashilatrasena:EzbOxZEmdJRPjZFv@apievidencia.ls3snwv.mongodb.net/post?retryWrites=true&w=majority&appName=APIevidencia',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-app.listen(10000);
+
+const connection = mongoose.connection; //creo conexion a la base de datos
+connection.once('open', () => {
+ console.log('MongoDB conexion de base de datos establecida');
+});
+
+app.listen(10000); //puerto donde se genera conexion
